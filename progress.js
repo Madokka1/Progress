2d191c97805de38da0fa7cdcd28c3bde785547d2
Madokka1 <152441132+Madokka1@users.noreply.github.com>
2024-12-24 14:46:52 +0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,12 +1,17 @@
-let totalAmount = 0;
+let progress = 0;
 
 export default function handler(req, res) {
+    // Настройка CORS для Tilda
+    res.setHeader('Access-Control-Allow-Origin', 'https://matroskin-perm.ru');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     if (req.method === 'GET') {
-        res.status(200).json({ amount: totalAmount });
+        res.status(200).json({ success: true, amount: progress });
     } 
     else if (req.method === 'POST') {
-        const amount = req.body.amount;
-        totalAmount += amount;
-        res.status(200).json({ newAmount: totalAmount });
+        const { amount } = req.body;
+        progress += parseInt(amount);
+        res.status(200).json({ success: true, newAmount: progress });
     }
 } 
